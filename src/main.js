a5c09950bfe5a471262f76cdf511d52a693e4a08
sretundijr <sretundijr@gmail.com>
2017-05-28 14:23:36 -0600

    webpack, express, mocha/chai, nodemon, setup

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+require("file-loader?name=index.html!./index.html");
+
+var hello = document.getElementById("hello");
+hello.innerHTML = "Hello World!";
+
+
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require("path");
+
+var DIST_DIR = path.join(__dirname, "dist"),
+    CLIENT_DIR = path.join(__dirname, "src");
+
+module.exports = {
+    context: CLIENT_DIR,
+
+    entry: "./main",
+
+    output: {
+        path: DIST_DIR,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
