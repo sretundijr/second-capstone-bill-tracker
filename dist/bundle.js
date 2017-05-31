8b9ae4fcb9108022db542136e8993eff5334ba86
sretundijr <sretundijr@gmail.com>
2017-05-30 21:43:42 -0600

    Modified package json

--- dist/bundle.js
+++ dist/bundle.js
@@ -80,8 +80,15 @@ console.log(HouseHolds[0].bills[0].name);
 var col_1_head = document.getElementById('col-head-1-js');
 var col_2_head = document.getElementById('col-head-2-js');
 
+var bills = HouseHolds[0].bills;
+var doubleIt = function doubleIt(bills) {
+  return bills.concat(bills.slice(0));
+};
+var lotsOfBills = doubleIt(doubleIt(doubleIt(bills)));
 col_1_head.innerHTML = HouseHolds[0].bills[0].name;
 col_2_head.innerHTML = HouseHolds[0].bills[0].dueDate;
+
+console.log(lotsOfBills);
 
 /***/ }),
 /* 1 */
--- src/app/client.js
+++ src/app/client.js
@@ -5,5 +5,10 @@ console.log(HouseHolds[0].bills[0].name);
 var col_1_head = document.getElementById('col-head-1-js');
 var col_2_head = document.getElementById('col-head-2-js');
 
+let bills = HouseHolds[0].bills;
+let doubleIt = (bills) => bills.concat(bills.slice(0))
+let lotsOfBills = doubleIt(doubleIt(doubleIt(bills)));
 col_1_head.innerHTML = HouseHolds[0].bills[0].name;
 col_2_head.innerHTML = HouseHolds[0].bills[0].dueDate;
+
+console.log(lotsOfBills);
